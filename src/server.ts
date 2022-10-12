253618e49fb7ffa70d7642255f81d56c9e468478
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-12 16:23:35 -0300

    feat: using  custom middleware of Error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { createConnection } from "./shared/infra/data-source";
+import { AppError } from "./shared/errors/AppError";
 createConnection()
 
 const app = express();
 app.use(express.json());
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Iternal server error - ${err.message}`
+  })
+})
 
 app.get("/", (req, res, next) => {
   res.json({
