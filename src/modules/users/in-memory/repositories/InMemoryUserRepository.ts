8b6c670e480a480770b9376117ab682f8547a6eb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-12 15:50:23 -0300

    feat: implemented Users Repository in memory

---
+++ src/modules/users/in-memory/repositories/InMemoryUserRepository.ts
@@ -0,0 +1,17 @@
+import { IUsersRepository } from "../../contracts/IUsersRepository";
+import { CreateUserDTO } from "../../DTO/CreateUserDTO";
+import { User } from "../../entities/User";
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  usersRepo: User[] = [];
+
+  async create(data: CreateUserDTO): Promise<void> {
+    const user = new User()
+
+    Object.assign(user, {
+      ...data
+    })
+
+    this.usersRepo.push(user)
+  }
+}
