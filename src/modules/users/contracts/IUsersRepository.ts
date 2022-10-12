9282fd9f40632aefd1dbea85b083c9c1d59d1656
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-12 16:01:42 -0300

    feat: implemented findByEmail method

--- src/modules/users/contracts/IUsersRepository.ts
+++ src/modules/users/contracts/IUsersRepository.ts
@@ -1,5 +1,7 @@
 import { CreateUserDTO } from "../DTO/CreateUserDTO";
+import { User } from "../entities/User";
 
 export interface IUsersRepository {
   create(data: CreateUserDTO): Promise<void>
+  findByEmail(username: string): Promise<User | undefined>
 }
