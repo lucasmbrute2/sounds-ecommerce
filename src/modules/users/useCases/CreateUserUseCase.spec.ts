ccfb7d22a810f8d63990fb2feded610880fa845c
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-14 09:04:27 -0300

    feat: Spec not be able to crerate a user with the same credentials

--- src/modules/users/useCases/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/CreateUserUseCase.spec.ts
@@ -1,15 +1,18 @@
 import { expect, test, describe, beforeEach } from "vitest"
+import { AppError } from "../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../in-memory/repositories/InMemoryUserRepository"
 import { CreateUserUseCase } from "./CreateUserUseCase"
 
+let usersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
 describe("Create a user", () => {
   beforeEach(() => {
-
+    usersRepository = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(usersRepository)
   })
 
   test("should be able to create a new user", async () => {
-    const userRepository = new InMemoryUsersRepository()
-    const createUserUseCase = new CreateUserUseCase(userRepository)
 
     await createUserUseCase.execute({
       first_name: 'test name',
@@ -19,8 +22,25 @@ describe("Create a user", () => {
       username: 'lucas@gmail.com'
     })
 
-    const user = await userRepository.findByEmail('lucas@gmail.com');
-
+    const user = await usersRepository.findByEmail('lucas@gmail.com');
     expect(user).toHaveProperty('id');
+  })
+
+  test("should not be able to create a user with existent credentials", () => {
+    const getUser = async () => {
+
+      const mockedUser = {
+        first_name: 'test name',
+        last_name: 'test lastname',
+        password: '12345',
+        phone: '123145',
+        username: 'lucas@gmail.com'
+      }
+
+      await createUserUseCase.execute(mockedUser)
+      await createUserUseCase.execute(mockedUser)
+    }
+
+    expect(getUser()).rejects.toBeInstanceOf(AppError)
   })
 })
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { createConnection } from "./shared/infra/data-source";
 import { AppError } from "./shared/errors/AppError";
