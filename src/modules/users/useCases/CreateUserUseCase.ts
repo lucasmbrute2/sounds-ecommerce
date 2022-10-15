c8a6799e45c0147b6c4678f3c70b493f82e029ab
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-15 12:01:08 -0300

    feat: implemented create user use case

---
+++ src/modules/users/useCases/CreateUserUseCase.ts
@@ -0,0 +1,22 @@
+import { AppError } from "../../../shared/errors/AppError";
+import { IUsersRepository } from "../contracts/IUsersRepository";
+import { CreateUserDTO } from "../DTO/CreateUserDTO";
+import { UserValidation } from "../DTO/UserValidation";
+
+export class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  async execute(data: CreateUserDTO) {
+    const userIsValid = await new UserValidation(data).validate();
+
+    if (!userIsValid) throw new AppError("Invalid fields");
+
+    const user = await this.usersRepository.findByEmail(data.username)
+
+    if (user) throw new AppError("User Already exists");
+
+    await this.usersRepository.create(data);
+
+    return data;
+  }
+} 
