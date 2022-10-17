e06bea9bf2badc177620782c6b422fd35f612a06
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-17 13:08:48 -0300

    feat: Created create user controller

---
+++ src/modules/users/useCases/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserDTO } from "../DTO/CreateUserDTO";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { first_name, last_name, password, phone, username }: CreateUserDTO = req.body;
+    const createUserUseCase = container.resolve(CreateUserUseCase)
+
+    await createUserUseCase.execute({
+      first_name,
+      last_name,
+      password,
+      phone,
+      username
+    })
+
+    return res.status(201).send();
+  }
+}
