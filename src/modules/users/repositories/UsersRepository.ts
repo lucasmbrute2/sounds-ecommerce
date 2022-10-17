a8bfad04c662da28539111ce9ceda29cee3fd406
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-17 12:14:20 -0300

    feat: Implemented Users Repository

---
+++ src/modules/users/repositories/UsersRepository.ts
@@ -0,0 +1,24 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../../shared/infra/data-source";
+import { IUsersRepository } from "../contracts/IUsersRepository";
+import { CreateUserDTO } from "../DTO/CreateUserDTO";
+import { User } from "../entities/User";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create(data: CreateUserDTO): Promise<void> {
+    const user = this.repository.create(data)
+    await this.repository.save(user)
+  }
+
+  async findByEmail(username: string): Promise<User | Falsy> {
+    return await this.repository.findOneBy({
+      username
+    })
+  }
+}
