4469dba6725272c187a9c81e9c905e2ecc51ba08
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-02 12:43:34 -0300

    chore: migration structure folder

--- src/shared/infra/migrations/1667403769303-default.ts
+++ src/shared/infra/migrations/1667403769303-default.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class migrations1667403521501 implements MigrationInterface {
-    name = 'migrations1667403521501'
+export class default1667403769303 implements MigrationInterface {
+    name = 'default1667403769303'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user_address" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "address_line1" character varying NOT NULL, "address_line2" character varying NOT NULL, "city" character varying NOT NULL, "postal_code" character varying NOT NULL, "country" character varying NOT NULL, "phone" character varying NOT NULL, "mobile" character varying NOT NULL, "user_id" uuid, CONSTRAINT "PK_302d96673413455481d5ff4022a" PRIMARY KEY ("id"))`);
