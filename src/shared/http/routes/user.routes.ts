c3e04a13214ac0bf49886602f6861d381baedffa
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-03 00:21:15 -0300

    feat: implemented create user address route

--- src/shared/http/routes/user.routes.ts
+++ src/shared/http/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateUserController } from "../../../modules/users/useCases/createUser/CreateUserController";
+import { CreateUserAddressController } from "../../../modules/users/useCases/createUserAddress/CreateUserAddressController";
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
+const createUserAddressController = new CreateUserAddressController();
 
 userRouter.post("/create", createUserController.handle)
+userRouter.post("/create/address/:user_id", createUserAddressController.handle)
 
 export { userRouter }
