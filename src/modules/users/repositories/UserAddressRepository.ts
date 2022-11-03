38ad174e31c74a52c370ea0952c536fa267a8ffc
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-03 00:20:22 -0300

    feat: Implemented user address repository

---
+++ src/modules/users/repositories/UserAddressRepository.ts
@@ -0,0 +1,28 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../../shared/infra/data-source";
+import { IUserAddressRespository } from "../contracts/IUserAddressRespository";
+import { CreateUserAddressDTO } from "../DTO/CreateUserDTO";
+import { User } from "../entities/User";
+import { UserAddress } from "../entities/UserAddress";
+
+export class UserAddressRepository implements IUserAddressRespository {
+    private repository: Repository<UserAddress>
+    
+    constructor(){
+        this.repository = AppDataSource.getRepository(UserAddress)
+    }
+    
+    async findByID(id: string): Promise<UserAddress | Falsy> {
+        return this.repository.findOneBy({id})
+    }
+
+    async createAddress(data: CreateUserAddressDTO, user:User): Promise<UserAddress> {
+        const userAddress = this.repository.create({
+            ...data,
+            user
+        })
+        
+        return await this.repository.save(userAddress);
+    }
+    
+}
