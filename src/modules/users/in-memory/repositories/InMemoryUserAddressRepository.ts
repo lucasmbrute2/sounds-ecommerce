75e4ad9686c498af1e80e18db880b6436e65881f
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-03 00:17:53 -0300

    feat: Implemented inmemory repository to CreateUser address

---
+++ src/modules/users/in-memory/repositories/InMemoryUserAddressRepository.ts
@@ -0,0 +1,26 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { IUserAddressRespository } from "../../contracts/IUserAddressRespository";
+import { CreateUserAddressDTO } from "../../DTO/CreateUserDTO";
+import { User } from "../../entities/User";
+import { UserAddress } from "../../entities/UserAddress";
+
+export class InMemoryUserAddressRepository implements IUserAddressRespository {
+    private repository: UserAddress[] = [];
+    
+    async findByID(id: string): Promise<Falsy | UserAddress> {
+        return this.repository.find(userAddress => userAddress.id === id)
+    }
+    
+    async createAddress(data: CreateUserAddressDTO, user:User): Promise<UserAddress> {
+        const userAddress = new UserAddress()
+        
+        Object.assign(userAddress, {
+            ...data,
+            user
+        })
+
+        this.repository.push(userAddress)
+        return this.repository.find(address => address.user === user) as UserAddress
+    }
+
+}
