995c2b6e6a690acee1e79464e7503b70ae88bb90
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-02 22:26:31 -0300

    feat: Created a separate contract to User address

---
+++ src/modules/users/contracts/IUserAddressRespository.ts
@@ -0,0 +1,8 @@
+import { CreateUserAddressDTO } from "../DTO/CreateUserDTO";
+import { User } from "../entities/User";
+import { UserAddress } from "../entities/UserAddress";
+
+export interface IUserAddressRespository {
+    createAddress(data: CreateUserAddressDTO, user:User): Promise<UserAddress>
+    findByID(id: string): Promise<UserAddress | Falsy>
+}
--- src/modules/users/contracts/IUsersRepository.ts
+++ src/modules/users/contracts/IUsersRepository.ts
@@ -4,6 +4,5 @@ import { User } from "../entities/User";
 export interface IUsersRepository {
   create(data: CreateUserDTO): Promise<void>
   findByEmail(username: string): Promise<User | Falsy>
-  createAddress(data: CreateUserAddressDTO): Promise<void>
   findByID(id: string):  Promise<User | Falsy>
 }
