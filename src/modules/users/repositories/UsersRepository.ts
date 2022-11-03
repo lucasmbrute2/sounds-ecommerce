7e37768b70ae2308ee098275c456e9be04fda4fb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-02 22:28:33 -0300

    feat: Implemented method findyByID & removed create address methods

--- src/modules/users/repositories/UsersRepository.ts
+++ src/modules/users/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../../../shared/infra/data-source";
 import { IUsersRepository } from "../contracts/IUsersRepository";
-import { CreateUserAddressDTO, CreateUserDTO } from "../DTO/CreateUserDTO";
+import { CreateUserDTO } from "../DTO/CreateUserDTO";
 import { User } from "../entities/User";
 
 export class UsersRepository implements IUsersRepository {
@@ -11,22 +11,18 @@ export class UsersRepository implements IUsersRepository {
     this.repository = AppDataSource.getRepository(User);
   }
   
-  async findByID(id: string): Promise<User | Falsy> {
-    throw new Error("Method not implemented.");
-  }
-
   async create(data: CreateUserDTO): Promise<void> {
     const user = this.repository.create(data)
     await this.repository.save(user)
+  }
+  
+  async findByID(id: string): Promise<User | Falsy> {
+    return await this.repository.findOneBy({ id })
   }
 
   async findByEmail(username: string): Promise<User | Falsy> {
     return await this.repository.findOneBy({
       username
     })
-  }
-  
-  async createAddress(data: CreateUserAddressDTO): Promise<void> {
-    throw new Error("Method not implemented.");
   }
 }
