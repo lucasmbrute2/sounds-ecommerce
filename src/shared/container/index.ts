b472b583aad234c978324a5240a68ba84e89584a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-02 22:25:27 -0300

    feat: Implemented container to UserAddress repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe"
+import { IUserAddressRespository } from "../../modules/users/contracts/IUserAddressRespository";
 import { IUsersRepository } from "../../modules/users/contracts/IUsersRepository"
+import { UserAddressRepository } from "../../modules/users/repositories/UserAddressRepository";
 import { UsersRepository } from "../../modules/users/repositories/UsersRepository"
 
 
@@ -7,3 +9,8 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<IUserAddressRespository>(
+  'UsersAddressRepository',
+  UserAddressRepository
+)
