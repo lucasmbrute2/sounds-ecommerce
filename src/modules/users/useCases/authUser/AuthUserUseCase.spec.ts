21ea3d4517657d8e82e0b0a96b89b7793334be06
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-15 13:47:01 -0300

    feat: implemented auth use case spec

--- src/modules/users/useCases/authUser/AuthUserUseCase.spec.ts
+++ src/modules/users/useCases/authUser/AuthUserUseCase.spec.ts
@@ -1,15 +1,18 @@
 import { expect, test, describe, beforeEach } from "vitest"
 import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../in-memory/repositories/InMemoryUserRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthUserUseCase } from "./AuthUserUseCase";
 
 let userRepository: InMemoryUsersRepository
 let authUserUseCase: AuthUserUseCase
+let createUserUseCase: CreateUserUseCase
 
 describe("Auth a user", ()=> {   
     beforeEach(()=> {
         userRepository = new InMemoryUsersRepository()
         authUserUseCase= new AuthUserUseCase(userRepository)
+        createUserUseCase = new CreateUserUseCase(userRepository)
     })    
 
     test("should not be able to auth a user without match password and confirm password", ()=> {
@@ -18,6 +21,24 @@ describe("Auth a user", ()=> {
                 username: 'username TEST',
                 password: 'password TEST',
                 confirmPassword: 'wrong password TEST'
+            })
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+    test("should not be able to auth a user with wrong password", ()=> {
+        expect(async ()=> {
+            await createUserUseCase.execute({
+                first_name: 'Lucas',
+                last_name: 'Dantas',
+                password: 'Adkad!321',
+                phone: '31313131',
+                username: 'emailtest@gmail.com'
+            })
+    
+            await authUserUseCase.execute({
+                password: 'Adkad!323',
+                confirmPassword: 'Adkad!323',
+                username: 'emailtest@gmail.com'
             })
         }).rejects.toBeInstanceOf(AppError)
     })
