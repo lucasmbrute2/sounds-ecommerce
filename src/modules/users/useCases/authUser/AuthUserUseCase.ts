0f8feb10a20dbfc34dcd5055a8fed5f7dd857b86
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-15 13:47:13 -0300

    feat: implemented auth use case

--- src/modules/users/useCases/authUser/AuthUserUseCase.ts
+++ src/modules/users/useCases/authUser/AuthUserUseCase.ts
@@ -1,4 +1,6 @@
-import { injectable } from "tsyringe";
+import { compare } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+import { User } from "../../../../entities/User";
 import { AppError } from "../../../../shared/errors/AppError";
 import { IUsersRepository } from "../../contracts/IUsersRepository";
 import { authUserDTO } from "../../DTO/CreateUserDTO";
@@ -6,11 +8,22 @@ import { authUserDTO } from "../../DTO/CreateUserDTO";
 @injectable()
 export class AuthUserUseCase {
     constructor(
+        @inject('UsersRepository')
         private usersRepository: IUsersRepository
     ){}
     
     async execute(data: authUserDTO){
-        const { password, confirmPassword } = data;
+        const { password, confirmPassword, username } = data;
         if (password !== confirmPassword) throw new AppError("Password don't match.")
+
+        const user = await this.usersRepository.findByEmail(username) as User
+        if (!user) throw new Error("User or password incorrect.");
+        
+        const passwordIsCorrect = await compare(password, user.password)
+        
+        if (!passwordIsCorrect) throw new AppError('Email or passsword incorrect, please try again.')
+        
+        //TODO return a JWT TOKEN
+        return 'jwt'
     }
 }
