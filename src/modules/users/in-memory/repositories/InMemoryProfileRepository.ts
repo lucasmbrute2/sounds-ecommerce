53884b023b3cd77ab777388a9fd60dc81efff253
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-22 20:58:20 -0300

    feat: created Inmemory profile repository

---
+++ src/modules/users/in-memory/repositories/InMemoryProfileRepository.ts
@@ -0,0 +1,20 @@
+import { Profile } from "../../../../entities/Profile";
+import { User } from "../../../../entities/User";
+import { IProfileRepository } from "../../contracts/IProfileRepository";
+
+export class InMemoryProfileRepository implements IProfileRepository {
+    private repository: Profile[] = [];
+    
+    async create(photo: string, user: User): Promise<Profile> {
+        const profile = new Profile()
+        profile.user = user;
+
+        Object.assign(profile, {
+            photo
+        })
+
+        this.repository.push(profile)
+        return profile;
+    }
+
+}
