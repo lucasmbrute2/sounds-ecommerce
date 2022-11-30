b21cb802882ba3682495a8440c862b88902723c2
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-11-30 08:17:17 -0300

    feat: Configuration multer to change file name

--- src/configs/uploads/upload.ts
+++ src/configs/uploads/upload.ts
@@ -1,7 +1,18 @@
+import multer from "multer";
 import { resolve } from "path";
+import crypto from "crypto"
 
-const tmpFolder = resolve(__dirname, '..', '..', '..', 'temp')
+const tmpFolder = resolve(__dirname, '..', '..', '..', 'tmp')
 
 export default {
     tmpFolder,
+    storage: multer.diskStorage({
+        destination: tmpFolder,
+        filename: (_, file, cb ) => {
+            const extension = file.originalname.split('.')[1]
+            const newFileName = crypto.randomBytes(64).toString('hex');
+
+            cb(null, `${newFileName}.${extension}`)
+        } 
+    })
 }
